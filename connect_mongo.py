c0bb6ca9a01095a1c80e95c2971679f49601ce07
Devin Pastoor <devin.pastoor@gmail.com>
2015-01-13 17:53:03 -0500

    add examples for searching and removing from db

--- connect_mongo.py
+++ connect_mongo.py
@@ -6,5 +6,17 @@ db = connection.meteor
 # get collection from database
 upload = db['cfs.uploads.filerecord']
 print(upload.find_one()['copies']['uploads']['name'])
+print(upload.find_one())
 
+# try removing object
+upload.remove({"copies.uploads.name":"21-144_Ketek_BioPharmr_P1.pdf"})
+
+# method of printing one
+# notice to search for nested documents you use the `.` to search
+print(upload.find_one({'copies.uploads.name':'42684061.PDF'}))
+
+#find returns cursor object so must iterator over it get actual info rather
+# than cursor object
+for u in upload.find({"_id": "xcbvufwBLRvdwaf6Q"}):
+    print(u)
 
