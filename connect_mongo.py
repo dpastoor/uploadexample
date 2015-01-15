b6f0390e562a9482b99504b6ead07989266718e5
Devin Pastoor <devin.pastoor@gmail.com>
2015-01-15 16:16:53 -0500

    see how to clear database

--- connect_mongo.py
+++ connect_mongo.py
@@ -9,14 +9,16 @@ print(upload.find_one()['copies']['uploads']['name'])
 print(upload.find_one())
 
 # try removing object
-upload.remove({"copies.uploads.name":"21-144_Ketek_BioPharmr_P1.pdf"})
-
+#upload.remove({"copies.uploads.name":"21-144_Ketek_BioPharmr_P1.pdf"})
+upload.remove({}) # to clear out db
+#upload.insert('')
 # method of printing one
 # notice to search for nested documents you use the `.` to search
-print(upload.find_one({'copies.uploads.name':'42684061.PDF'}))
+
+#print(upload.find_one({'copies.uploads.name':'42684061.PDF'}))
 
 #find returns cursor object so must iterator over it get actual info rather
 # than cursor object
-for u in upload.find({"_id": "xcbvufwBLRvdwaf6Q"}):
-    print(u)
+#for u in upload.find({"_id": "xcbvufwBLRvdwaf6Q"}):
+#    print(u)
 
